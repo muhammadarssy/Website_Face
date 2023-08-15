04754554ca73b802a6928b564d030eee25458815
muhammadarssy <muhammadarssy@gmail.com>
2023-08-15 08:26:25 +0700

    add commit

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Dashboard;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class DashboardController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = dashboard::orderBy('id', 'DESC')->get();
+        $last = dashboard::orderBy('id', 'DESC')->first();
+        //
+        return view('dashboard', compact('data', 'last'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Dashboard $dashboard)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Dashboard $dashboard)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Dashboard $dashboard)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Dashboard $dashboard)
+    {
+        //
+    }
+}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,474 @@
+<!--
+=========================================================
+* Corporate UI - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/corporate-ui
+* Copyright 2022 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://www.creative-tim.com/license)
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+-->
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <title>
+    Face Recognition
+  </title>
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|Noto+Sans:300,400,500,600,700,800|PT+Mono:300,400,500,600,700" rel="stylesheet" />
+  <!-- Nucleo Icons -->
+  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- Font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/349ee9c857.js" crossorigin="anonymous"></script>
+  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <!-- CSS Files -->
+  <link id="pagestyle" href="../assets/css/corporate-ui-dashboard.css?v=1.0.0" rel="stylesheet" />
+</head>
+
+<body class="g-sidenav-show  bg-gray-100">
+  <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+    <!-- Navbar -->
+    <nav class="navbar navbar-main navbar-expand-lg mx-5 px-0 shadow-none rounded" id="navbarBlur" navbar-scroll="true">
+      <div class="container-fluid py-1 px-2">
+        <nav aria-label="breadcrumb">
+          <ol class="breadcrumb bg-transparent mb-1 pb-0 pt-1 px-0 me-sm-6 me-5">
+            <li class="breadcrumb-item text-sm"><a class="opacity-5 text-dark" href="javascript:;">Dashboard</a></li>
+            <li class="breadcrumb-item text-sm text-dark active" aria-current="page">Dashboard</li>
+          </ol>
+          <h6 class="font-weight-bold mb-0">Dashboard</h6>
+        </nav>
+      </div>
+    </nav>
+    <!-- End Navbar -->
+    <div class="container-fluid py-4 px-5">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="d-md-flex align-items-center mb-3 mx-2">
+            <div class="mb-md-0 mb-3">
+              <h3 class="font-weight-bold mb-0">Hello, Frisca</h3>
+              <p class="mb-0">Face System Detection!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr class="my-0">
+      <div class="row">
+        <div class="col-xl-4 col-sm-6 mb-xl-0">
+          <div class="card border shadow-xs mb-4">
+            <div class="card-body text-start p-3 w-100">
+              <div class="icon icon-shape icon-sm bg-dark text-white text-center border-radius-sm d-flex align-items-center justify-content-center mb-3">
+                <svg height="16" width="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                  <path d="M4.5 3.75a3 3 0 00-3 3v.75h21v-.75a3 3 0 00-3-3h-15z" />
+                  <path fill-rule="evenodd" d="M22.5 9.75h-21v7.5a3 3 0 003 3h15a3 3 0 003-3v-7.5zm-18 3.75a.75.75 0 01.75-.75h6a.75.75 0 010 1.5h-6a.75.75 0 01-.75-.75zm.75 2.25a.75.75 0 000 1.5h3a.75.75 0 000-1.5h-3z" clip-rule="evenodd" />
+                </svg>
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  <div class="w-100">
+                    <p class="text-sm text-secondary mb-1">Transactions</p>
+                    <h4 class="mb-2 font-weight-bold">{{$last->transaction}}</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-4 col-sm-6 mb-xl-0">
+          <div class="card border shadow-xs mb-4">
+            <div class="card-body text-start p-3 w-100">
+              <div class="icon icon-shape icon-sm bg-dark text-white text-center border-radius-sm d-flex align-items-center justify-content-center mb-3">
+                <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                  <path fill-rule="evenodd" d="M7.5 5.25a3 3 0 013-3h3a3 3 0 013 3v.205c.933.085 1.857.197 2.774.334 1.454.218 2.476 1.483 2.476 2.917v3.033c0 1.211-.734 2.352-1.936 2.752A24.726 24.726 0 0112 15.75c-2.73 0-5.357-.442-7.814-1.259-1.202-.4-1.936-1.541-1.936-2.752V8.706c0-1.434 1.022-2.7 2.476-2.917A48.814 48.814 0 017.5 5.455V5.25zm7.5 0v.09a49.488 49.488 0 00-6 0v-.09a1.5 1.5 0 011.5-1.5h3a1.5 1.5 0 011.5 1.5zm-3 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd" />
+                  <path d="M3 18.4v-2.796a4.3 4.3 0 00.713.31A26.226 26.226 0 0012 17.25c2.892 0 5.68-.468 8.287-1.335.252-.084.49-.189.713-.311V18.4c0 1.452-1.047 2.728-2.523 2.923-2.12.282-4.282.427-6.477.427a49.19 49.19 0 01-6.477-.427C4.047 21.128 3 19.852 3 18.4z" />
+                </svg>
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  <div class="w-100">
+                    <p class="text-sm text-secondary mb-1">Account</p>
+                    <h4 class="mb-2 font-weight-bold">{{$last->account}}</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-xl-4 col-sm-6">
+          <div class="card border shadow-xs mb-4">
+            <div class="card-body text-start p-3 w-100">
+              <div class="icon icon-shape icon-sm bg-dark text-white text-center border-radius-sm d-flex align-items-center justify-content-center mb-3">
+                <svg width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                  <path fill-rule="evenodd" d="M5.25 2.25a3 3 0 00-3 3v4.318a3 3 0 00.879 2.121l9.58 9.581c.92.92 2.39 1.186 3.548.428a18.849 18.849 0 005.441-5.44c.758-1.16.492-2.629-.428-3.548l-9.58-9.581a3 3 0 00-2.122-.879H5.25zM6.375 7.5a1.125 1.125 0 100-2.25 1.125 1.125 0 000 2.25z" clip-rule="evenodd" />
+                </svg>
+              </div>
+              <div class="row">
+                <div class="col-12">
+                  <div class="w-100">
+                    <p class="text-sm text-secondary mb-1">Date</p>
+                    <h4 class="mb-2 font-weight-bold">{{$last->created_at}}</h4>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row my-4">
+        <div class="col-lg-12 col-md-6">
+          <div class="card shadow-xs border">
+            <div class="card-header border-bottom pb-0">
+              <div class="d-sm-flex align-items-center mb-3">
+                <div>
+                  <h6 class="font-weight-semibold text-lg mb-0">Recent transactions</h6>
+                  <p class="text-sm mb-sm-0 mb-2">Siapa yang membuka brankas</p>
+                </div>
+              </div>
+            </div>
+            <div class="card-body px-0 py-0">
+              <div class="table-responsive p-0">
+                <table class="table align-items-center justify-content-center mb-0">
+                  <thead class="bg-gray-100">
+                    <tr>
+                      <th class="text-secondary text-xs font-weight-semibold opacity-7">Transaction</th>
+                      <th class="text-secondary text-xs font-weight-semibold opacity-7 ps-2">Account</th>
+                      <th class="text-secondary text-xs font-weight-semibold opacity-7 ps-2">Date</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @forelse ($data as $data)
+                    <tr>
+                      <td>
+                        <div class="d-flex px-2">
+                          <div class="my-auto">
+                            <h6 class="mb-0 text-sm">{{$data->transaction}}</h6>
+                          </div>
+                        </div>
+                      </td>
+                      <td>
+                        <p class="text-sm font-weight-normal mb-0">{{$data->account}}</p>
+                      </td>
+                      <td>
+                        <span class="text-sm font-weight-normal">{{$data->created_at}}</span>
+                      </td>
+                    </tr>
+                    @empty
+                    <tr>
+                      <td>
+                        <div class="d-flex px-2">
+                          <div class="my-auto">
+                            <h6 class="mb-0 text-sm">Data Kosong</h6>
+                          </div>
+                        </div>
+                      </td>
+                      <td>
+                        <p class="text-sm font-weight-normal mb-0">Data Kosong</p>
+                      </td>
+                      <td>
+                        <span class="text-sm font-weight-normal">Data Kosong</span>
+                      </td>
+                    </tr>
+                    @endforelse
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <footer class="footer pt-3  ">
+        <div class="container-fluid">
+          <div class="row align-items-center justify-content-lg-between">
+            <div class="col-lg-6 mb-lg-0 mb-4">
+              <div class="copyright text-center text-xs text-muted text-lg-start">
+                Copyright
+                © <script>
+                  document.write(new Date().getFullYear())
+                </script>
+                Face Recognition
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  </main>
+  <!--   Core JS Files   -->
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/chartjs.min.js"></script>
+  <script src="../assets/js/plugins/swiper-bundle.min.js" type="text/javascript"></script>
+  <script>
+    if (document.getElementsByClassName('mySwiper')) {
+      var swiper = new Swiper(".mySwiper", {
+        effect: "cards",
+        grabCursor: true,
+        initialSlide: 1,
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+    };
+
+
+    var ctx = document.getElementById("chart-bars").getContext("2d");
+
+    new Chart(ctx, {
+      type: "bar",
+      data: {
+        labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"],
+        datasets: [{
+            label: "Sales",
+            tension: 0.4,
+            borderWidth: 0,
+            borderSkipped: false,
+            backgroundColor: "#2ca8ff",
+            data: [450, 200, 100, 220, 500, 100, 400, 230, 500, 200],
+            maxBarThickness: 6
+          },
+          {
+            label: "Sales",
+            tension: 0.4,
+            borderWidth: 0,
+            borderSkipped: false,
+            backgroundColor: "#7c3aed",
+            data: [200, 300, 200, 420, 400, 200, 300, 430, 400, 300],
+            maxBarThickness: 6
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: false,
+          },
+          tooltip: {
+            backgroundColor: '#fff',
+            titleColor: '#1e293b',
+            bodyColor: '#1e293b',
+            borderColor: '#e9ecef',
+            borderWidth: 1,
+            usePointStyle: true
+          }
+        },
+        interaction: {
+          intersect: false,
+          mode: 'index',
+        },
+        scales: {
+          y: {
+            stacked: true,
+            grid: {
+              drawBorder: false,
+              display: true,
+              drawOnChartArea: true,
+              drawTicks: false,
+              borderDash: [4, 4],
+            },
+            ticks: {
+              beginAtZero: true,
+              padding: 10,
+              font: {
+                size: 12,
+                family: "Noto Sans",
+                style: 'normal',
+                lineHeight: 2
+              },
+              color: "#64748B"
+            },
+          },
+          x: {
+            stacked: true,
+            grid: {
+              drawBorder: false,
+              display: false,
+              drawOnChartArea: false,
+              drawTicks: false
+            },
+            ticks: {
+              font: {
+                size: 12,
+                family: "Noto Sans",
+                style: 'normal',
+                lineHeight: 2
+              },
+              color: "#64748B"
+            },
+          },
+        },
+      },
+    });
+
+
+    var ctx2 = document.getElementById("chart-line").getContext("2d");
+
+    var gradientStroke1 = ctx2.createLinearGradient(0, 230, 0, 50);
+
+    gradientStroke1.addColorStop(1, 'rgba(45,168,255,0.2)');
+    gradientStroke1.addColorStop(0.2, 'rgba(45,168,255,0.0)');
+    gradientStroke1.addColorStop(0, 'rgba(45,168,255,0)'); //blue colors
+
+    var gradientStroke2 = ctx2.createLinearGradient(0, 230, 0, 50);
+
+    gradientStroke2.addColorStop(1, 'rgba(119,77,211,0.4)');
+    gradientStroke2.addColorStop(0.7, 'rgba(119,77,211,0.1)');
+    gradientStroke2.addColorStop(0, 'rgba(119,77,211,0)'); //purple colors
+
+    new Chart(ctx2, {
+      plugins: [{
+        beforeInit(chart) {
+          const originalFit = chart.legend.fit;
+          chart.legend.fit = function fit() {
+            originalFit.bind(chart.legend)();
+            this.height += 40;
+          }
+        },
+      }],
+      type: "line",
+      data: {
+        labels: ["Aug 18", "Aug 19", "Aug 20", "Aug 21", "Aug 22", "Aug 23", "Aug 24", "Aug 25", "Aug 26", "Aug 27", "Aug 28", "Aug 29", "Aug 30", "Aug 31", "Sept 01", "Sept 02", "Sept 03", "Aug 22", "Sept 04", "Sept 05", "Sept 06", "Sept 07", "Sept 08", "Sept 09"],
+        datasets: [{
+            label: "Volume",
+            tension: 0,
+            borderWidth: 2,
+            pointRadius: 3,
+            borderColor: "#2ca8ff",
+            pointBorderColor: '#2ca8ff',
+            pointBackgroundColor: '#2ca8ff',
+            backgroundColor: gradientStroke1,
+            fill: true,
+            data: [2828, 1291, 3360, 3223, 1630, 980, 2059, 3092, 1831, 1842, 1902, 1478, 1123, 2444, 2636, 2593, 2885, 1764, 898, 1356, 2573, 3382, 2858, 4228],
+            maxBarThickness: 6
+
+          },
+          {
+            label: "Trade",
+            tension: 0,
+            borderWidth: 2,
+            pointRadius: 3,
+            borderColor: "#832bf9",
+            pointBorderColor: '#832bf9',
+            pointBackgroundColor: '#832bf9',
+            backgroundColor: gradientStroke2,
+            fill: true,
+            data: [2797, 2182, 1069, 2098, 3309, 3881, 2059, 3239, 6215, 2185, 2115, 5430, 4648, 2444, 2161, 3018, 1153, 1068, 2192, 1152, 2129, 1396, 2067, 1215, 712, 2462, 1669, 2360, 2787, 861],
+            maxBarThickness: 6
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: true,
+            position: 'top',
+            align: 'end',
+            labels: {
+              boxWidth: 6,
+              boxHeight: 6,
+              padding: 20,
+              pointStyle: 'circle',
+              borderRadius: 50,
+              usePointStyle: true,
+              font: {
+                weight: 400,
+              },
+            },
+          },
+          tooltip: {
+            backgroundColor: '#fff',
+            titleColor: '#1e293b',
+            bodyColor: '#1e293b',
+            borderColor: '#e9ecef',
+            borderWidth: 1,
+            pointRadius: 2,
+            usePointStyle: true,
+            boxWidth: 8,
+          }
+        },
+        interaction: {
+          intersect: false,
+          mode: 'index',
+        },
+        scales: {
+          y: {
+            grid: {
+              drawBorder: false,
+              display: true,
+              drawOnChartArea: true,
+              drawTicks: false,
+              borderDash: [4, 4]
+            },
+            ticks: {
+              callback: function(value, index, ticks) {
+                return parseInt(value).toLocaleString() + ' EUR';
+              },
+              display: true,
+              padding: 10,
+              color: '#b2b9bf',
+              font: {
+                size: 12,
+                family: "Noto Sans",
+                style: 'normal',
+                lineHeight: 2
+              },
+              color: "#64748B"
+            }
+          },
+          x: {
+            grid: {
+              drawBorder: false,
+              display: false,
+              drawOnChartArea: false,
+              drawTicks: false,
+              borderDash: [4, 4]
+            },
+            ticks: {
+              display: true,
+              color: '#b2b9bf',
+              padding: 20,
+              font: {
+                size: 12,
+                family: "Noto Sans",
+                style: 'normal',
+                lineHeight: 2
+              },
+              color: "#64748B"
+            }
+          },
+        },
+      },
+    });
+  </script>
+  <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Corporate UI Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="../assets/js/corporate-ui-dashboard.min.js?v=1.0.0"></script>
+</body>
+
+</html>
